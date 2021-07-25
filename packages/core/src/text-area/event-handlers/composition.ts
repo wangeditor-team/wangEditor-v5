fd4b8f380f0e98dfe37c66da7336e754ee7bf1f0
leohai <448456376@qq.com>
2021-07-25 17:16:52 +0800

    fix: 更换文字样式时，中文输入文字重复

--- packages/core/src/text-area/event-handlers/composition.ts
+++ packages/core/src/text-area/event-handlers/composition.ts
@@ -3,12 +3,14 @@
  * @author wangfupeng
  */
 
-import { Editor, Range } from 'slate'
+import { Editor, Range, Node } from 'slate'
+import { VNode } from 'snabbdom'
 import { IDomEditor } from '../../editor/interface'
 import { DomEditor } from '../../editor/dom-editor'
 import TextArea from '../TextArea'
 import { hasEditableTarget } from '../helpers'
 import { IS_SAFARI, IS_FIREFOX_LEGACY, IS_CHROME } from '../../utils/ua'
+import { TEXTAREA_TO_VNODE } from '../../utils/weak-maps'
 
 const EDITOR_TO_TEXT: WeakMap<IDomEditor, string> = new WeakMap()
 
@@ -46,7 +48,6 @@ export function handleCompositionEnd(e: Event, textarea: TextArea, editor: IDomE
   if (selection == null) return
 
   const { data } = event
-
   // 检查 maxLength
   //【注意】这里只处理拼音输入的 maxLength 限制，英文、数组的限制，在 editor.insertText 中处理
   if (DomEditor.checkMaxLength(editor, data)) {
@@ -61,6 +62,21 @@ export function handleCompositionEnd(e: Event, textarea: TextArea, editor: IDomE
   // type that we need. So instead, insert whenever a composition
   // ends since it will already have been committed to the DOM.
   if (!IS_SAFARI && !IS_FIREFOX_LEGACY && data) {
+    // 强行修改相应结点的vnode的key，让snabbdom patch 时对插入中文的text-node进行渲染更新
+    let oldVnode = TEXTAREA_TO_VNODE.get(textarea)
+    let nodePath = selection.anchor.path
+    const parentNode = Node.parent(editor, nodePath)
+    // code 代码块不进行处理
+    if (oldVnode != null && parentNode.type != 'code') {
+      for (let i = 0; i < nodePath.length; i++) {
+        // table 的vnode结构有点特殊，table标签后还有个tbody，table-row开始才能根据path路径匹配
+        if (oldVnode.sel === 'table') {
+          oldVnode = oldVnode.children![0] as VNode
+        }
+        oldVnode = oldVnode!.children![nodePath[i]] as VNode
+      }
+      oldVnode.key = '' // 修改匹配后vnode的key
+    }
     Editor.insertText(editor, data)
   }
 
