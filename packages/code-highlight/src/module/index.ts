4ee82b643142ee81e2a6713eec7d5c646a1e2eb4
luochao <luochao@joyy.sg>
2021-08-01 21:35:39 +0800

    workflow: 测试发版

--- packages/code-highlight/src/module/index.ts
+++ packages/code-highlight/src/module/index.ts
@@ -3,7 +3,7 @@
  * @author wangfupeng
  */
 
-import { IModuleConf } from 'wangeditor-ore'
+import { IModuleConf } from 'wangeditor-core'
 import { renderTextStyle } from './render-text-style'
 import { selectLangMenuConf } from './menu/index'
 import { codeToHtmlConf } from './elem-to-html'
