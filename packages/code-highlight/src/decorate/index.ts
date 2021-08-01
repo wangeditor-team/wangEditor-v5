cfb7bb16effd09452b12668d567b8d545dec80d0
luochao <luochao@joyy.sg>
2021-08-01 21:50:24 +0800

    workflow: 测试发版

--- packages/code-highlight/src/decorate/index.ts
+++ packages/code-highlight/src/decorate/index.ts
@@ -4,7 +4,7 @@
  */
 
 import { Node, NodeEntry, Range, Text } from 'slate'
-import { DomEditor } from 'wangeditor-ore'
+import { DomEditor } from 'wangeditor-core'
 import { getPrismTokens, getPrismTokenLength } from '../vendor/prism'
 import { CodeElement } from '../custom-types'
 
