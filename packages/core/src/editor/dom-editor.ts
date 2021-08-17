de55b188d3c68cd60829e1d2268c2ee5e321a948
echoLC <651644100@qq.com>
2021-08-18 00:06:25 +0800

    workflow: 优化打包

--- packages/core/src/editor/dom-editor.ts
+++ packages/core/src/editor/dom-editor.ts
@@ -8,7 +8,7 @@ import toArray from 'lodash.toarray'
 import { Editor, Node, Element, Path, Point, Range, Ancestor, Text } from 'slate'
 import { IDomEditor } from './interface'
 import { Key } from '../utils/key'
-import TextArea from '../text-area/TextArea'
+import type TextArea from '../text-area/TextArea'
 import Toolbar from '../menus/bar/Toolbar'
 import HoverBar from '../menus/bar/HoverBar'
 import {
